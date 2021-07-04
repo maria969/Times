548245a34c2c41fc0efd3363f48a474e1abb15f2
Maria Herrero Varas <mherrero@divisait.com>
2021-07-04 22:14:48 +0200

    Added Search Result View Controller and Presenter

---
+++ Times/Presenter/SearchResult/SearchResultPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultPresenter.swift
+//  Times
+//
+//  Created by Maria Herrero Varas on 04/07/2021.
+//  
+//
+
+import Foundation
+
+//MARK: - SearchResultPresenterInterface
+
+class SearchResultPresenter: Presenter<SearchResultViewController>, SearchResultPresenterInterface {
+    
+    //MARK: - Public Properties
+    
+    //MARK: - Private Properties
+    
+    //MARK: - Public Methods
+}
---
+++ Times/Presenter/SearchResult/SearchResultPresenterInterface.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultPresenterInterface.swift
+//  Times
+//
+//  Created by Maria Herrero Varas on 04/07/2021.
+//  
+//
+
+import Foundation
+
+protocol SearchResultPresenterInterface: PresenterInterface {
+}
