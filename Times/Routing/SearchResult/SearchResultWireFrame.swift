1c3e70c8fae2f05a24641ff4cdf185bf8b0237ff
Maria Herrero Varas <mherrero@divisait.com>
2021-07-04 22:18:54 +0200

    Updated Result WireFrame

--- Times/Routing/SearchResult/SearchResultWireFrame.swift
+++ Times/Routing/SearchResult/SearchResultWireFrame.swift
@@ -16,6 +16,12 @@ public class SearchResultWireFrame: SearchResultWireFrameInterface {
     //MARK: - Public Methods
     
     public func presentDetail(article: ArticleEntity, fromView view: SearchResultViewControllerInterface) {
-        //TODO: - Present Detail
+        let viewController: UIViewController = view.viewController
+        guard let navigationController: UINavigationController = viewController.navigationController else { return }
+        
+        let detailVC: ArticleViewControllerInterface = Resolver.resolve()
+        detailVC.assign(article: article)
+        
+        navigationController.pushViewController(detailVC.viewController, animated: true)
     }
 }
